f2599a13a2b401edff36503f1055dcb8b3e5bcd6
MustafaAta <65856585+AtaRekt@users.noreply.github.com>
2024-12-29 03:39:50 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className='h-full'>
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "pkrioaabaa");
+            `
+          }}
+        />
+      </head>
       <body className={cn(inter.className, "antialiased bg-white h-full")}>
         <AudioUploadProvider>
           <QueryProvider>
